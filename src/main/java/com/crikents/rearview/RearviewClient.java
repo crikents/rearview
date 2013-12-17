04bd08925255b0060b36803342bf4e058a525a4c
crikents <crikents@crikents.com>
2013-12-16 20:09:13 -0800

    Fix scaling issue.

--- src/main/java/com/crikents/rearview/RearviewClient.java
+++ src/main/java/com/crikents/rearview/RearviewClient.java
@@ -13,13 +13,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagByte;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.common.MinecraftForge;
 import org.lwjgl.opengl.ARBFramebufferObject;
 import org.lwjgl.opengl.GL11;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.EnumSet;
 
 /**
@@ -51,9 +48,9 @@ public class RearviewClient implements Rearview, ITickHandler {
 
         try {
             renderEndNanoTime = EntityRenderer.class.getDeclaredField("renderEndNanoTime");
+        } catch (Exception e) {}
+        if (renderEndNanoTime != null) {
             renderEndNanoTime.setAccessible(true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -84,7 +81,7 @@ public class RearviewClient implements Rearview, ITickHandler {
     public void updateMirror(float partialTick) {
         int w, h;
         float y, py, p, pp;
-        boolean hide;
+        boolean hide, pause;
         int view, limit;
         long endTime = 0;
         MovingObjectPosition mouseOver;
@@ -101,9 +98,11 @@ public class RearviewClient implements Rearview, ITickHandler {
 
         switchToFB();
 
-        try {
-            endTime = renderEndNanoTime.getLong(mc.entityRenderer);
-        } catch (Exception e) {}
+        if (renderEndNanoTime != null) {
+            try {
+                endTime = renderEndNanoTime.getLong(mc.entityRenderer);
+            } catch (Exception e) {}
+        }
         mc.displayHeight = 180;
         mc.displayWidth = 320;
         mc.gameSettings.hideGUI = true;
@@ -111,14 +110,18 @@ public class RearviewClient implements Rearview, ITickHandler {
         mc.gameSettings.limitFramerate = 0;
         mc.renderViewEntity.rotationYaw += 180;
         mc.renderViewEntity.prevRotationYaw += 180;
-        mc.renderViewEntity.rotationPitch = -p + 15;
-        mc.renderViewEntity.prevRotationPitch = -pp + 15;
+        mc.renderViewEntity.rotationPitch = -p + 18;
+        mc.renderViewEntity.prevRotationPitch = -pp + 18;
 
-        GL11.glPushAttrib(GL11.GL_VIEWPORT_BIT | GL11.GL_ENABLE_BIT | GL11.GL_CURRENT_BIT | GL11.GL_POLYGON_BIT | GL11.GL_TEXTURE_BIT);
+        GL11.glPushAttrib(GL11.GL_VIEWPORT_BIT | GL11.GL_ENABLE_BIT |
+                          GL11.GL_CURRENT_BIT | GL11.GL_POLYGON_BIT |
+                          GL11.GL_TEXTURE_BIT);
         mc.entityRenderer.updateCameraAndRender(partialTick);
-        try {
-            renderEndNanoTime.setLong(mc.entityRenderer, endTime);
-        } catch (Exception e) {}
+        if (renderEndNanoTime != null) {
+            try {
+                renderEndNanoTime.setLong(mc.entityRenderer, endTime);
+            } catch (Exception e) {}
+        }
         GL11.glPopAttrib();
 
         mc.objectMouseOver = mouseOver;
@@ -133,8 +136,6 @@ public class RearviewClient implements Rearview, ITickHandler {
         mc.displayHeight = h;
 
         switchFromFB();
-
-        mc.entityRenderer.setupOverlayRendering();
     }
 
     @Override
@@ -151,15 +152,20 @@ public class RearviewClient implements Rearview, ITickHandler {
 
         boolean onLeft = mirrorSideNbt.data == 0;
 
-        if (framecount++%2 == 0) {
-            updateMirror((Float)objects[0]);
+        if (framecount++ % 2 == 0) {
+            updateMirror((Float) objects[0]);
         }
 
-        GL11.glPushMatrix();
         GL11.glPushAttrib(GL11.GL_ENABLE_BIT | GL11.GL_CURRENT_BIT | GL11.GL_POLYGON_BIT | GL11.GL_TEXTURE_BIT);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, mc.displayWidth, mc.displayHeight, 0, 1000, 3000);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glLoadIdentity();
+        GL11.glTranslatef(0, 0, -2000);
 
         if (!onLeft) {
-            GL11.glTranslatef(mc.displayWidth / 2, 0f, 0f);
+            GL11.glTranslatef(mc.displayWidth, 0f, 0f);
             GL11.glScalef(-1f, 1, 1);
             GL11.glFrontFace(GL11.GL_CW);
         }
@@ -169,31 +175,31 @@ public class RearviewClient implements Rearview, ITickHandler {
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glColor3ub((byte) 24, (byte) 24, (byte) 24);
         tes.startDrawing(GL11.GL_QUADS);
-        tes.addVertex(0, mc.displayHeight / 30, 0);
-        tes.addVertex(0, mc.displayHeight / 20, 0);
-        tes.addVertex(mc.displayWidth / 20, mc.displayHeight / 15, 0);
-        tes.addVertex(mc.displayWidth / 20, mc.displayHeight / 25, 0);
+        tes.addVertex(0, mc.displayHeight / 15, 0);
+        tes.addVertex(0, mc.displayHeight / 10, 0);
+        tes.addVertex(mc.displayWidth / 10, mc.displayHeight / 7.5, 0);
+        tes.addVertex(mc.displayWidth / 10, mc.displayHeight / 12.5, 0);
         tes.draw();
 
         tes.startDrawing(GL11.GL_QUADS);
-        tes.addVertex(mc.displayWidth / 68, mc.displayHeight / 78, 0);
-        tes.addVertex(mc.displayWidth / 68, mc.displayHeight / 5.8, 0);
-        tes.addVertex(mc.displayWidth / 5.93, mc.displayHeight / 6.9, 0);
-        tes.addVertex(mc.displayWidth / 5.93, mc.displayHeight / 48, 0);
+        tes.addVertex(mc.displayWidth / 34, mc.displayHeight / 39, 0);
+        tes.addVertex(mc.displayWidth / 34, mc.displayHeight / 2.9, 0);
+        tes.addVertex(mc.displayWidth / 2.965, mc.displayHeight / 3.45, 0);
+        tes.addVertex(mc.displayWidth / 2.965, mc.displayHeight / 24, 0);
         tes.draw();
 
         GL11.glColor3ub((byte) 255, (byte) 255, (byte) 255);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, mirrorTex);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         tes.startDrawing(GL11.GL_QUADS);
-        tes.addVertexWithUV(mc.displayWidth / 60, mc.displayHeight / 60, 0, onLeft ? 1 : 0, 1);
-        tes.addVertexWithUV(mc.displayWidth / 60, mc.displayHeight / 6, 0.2, onLeft ? 1 : 0, 0);
-        tes.addVertexWithUV(mc.displayWidth / 6, mc.displayHeight / 7, 0.4, onLeft ? 0 : 1, 0);
-        tes.addVertexWithUV(mc.displayWidth / 6, mc.displayHeight / 40, 0.3, onLeft ? 0 : 1, 1);
+        tes.addVertexWithUV(mc.displayWidth / 30, mc.displayHeight / 30, 0, onLeft ? 1 : 0, 1);
+        tes.addVertexWithUV(mc.displayWidth / 30, mc.displayHeight / 3, 0.2, onLeft ? 1 : 0, 0);
+        tes.addVertexWithUV(mc.displayWidth / 3, mc.displayHeight / 3.5, 0.4, onLeft ? 0 : 1, 0);
+        tes.addVertexWithUV(mc.displayWidth / 3, mc.displayHeight / 20, 0.3, onLeft ? 0 : 1, 1);
         tes.draw();
 
         GL11.glPopAttrib();
-        GL11.glPopMatrix();
+        mc.entityRenderer.setupOverlayRendering();
     }
 
     @Override
